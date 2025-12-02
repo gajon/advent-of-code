625eae74dd511be25f548c4b33dc627cc90c4233
Jorge Gajon <gajon@gajon.org>
2025-12-01 21:05:26 -0600

    AoC 2025 Day 01 - Part 1

---
+++ 2025/day01/solution-part-one.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+class Dial
+  attr_reader :position
+
+  def initialize
+    @position = 50
+  end
+
+  def new_position(movement)
+    _, direction, steps = movement.split(/(\D)(\d+)/)
+    direction == "L" ? left(steps.to_i) : right(steps.to_i)
+  end
+
+  def left(n)
+    @position = @position - n
+
+    while @position < 0 do; @position = @position + 100; end
+
+    @position
+  end
+
+  def right(n)
+    @position = @position + n
+
+    while @position >= 100 do; @position = @position - 100; end
+
+    @position
+  end
+end
+
+def decipher(dial, movements)
+  movements
+    .map { |mov| dial.new_position(mov) }
+    .select { |position| position.zero? }
+    .count
+end
+
+File.open("input", "r") do |file|
+  movements = file.readlines
+  puts "Movements in the file #{movements.size}"
+
+  password = decipher(Dial.new, movements)
+
+  puts "Password to open the door: #{password}"
+end
