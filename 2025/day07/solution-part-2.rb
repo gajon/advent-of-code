c5e2daaf3b477fab3ba3a8a4b65951f8c921f27e
Jorge Gajon <gajon@gajon.org>
2025-12-28 17:29:46 -0600

    AoC 2025 Day 07 - Part 2 (final solution)

--- 2025/day07/solution-part-2.rb
+++ 2025/day07/solution-part-2.rb
@@ -11,22 +11,22 @@ def count_timelines_slow!(beam_position, rows)
   if this_level[beam_position] == "^"
     if beam_position > 0
       # Beam splits to the left
-      count = count_timelines(beam_position - 1, rest)
+      count = count_timelines_slow!(beam_position - 1, rest)
     end
     if beam_position < this_level.size - 1
       # Beam splits to the right
-      count += count_timelines(beam_position + 1, rest)
+      count += count_timelines_slow!(beam_position + 1, rest)
     end
     count
   else
-    count_timelines(beam_position, rest)
+    count_timelines_slow!(beam_position, rest)
   end
 end
 
 # The above solution is very slow. This next one is traverses the levels only
 # once, from top to bottom. But it builds a list of all possible timelines, and
 # it grows excessively large. I'm discarding this solution too.
-def split_timelines(timelines, next_steps)
+def split_timelines_grows_excessively!(timelines, next_steps)
   return timelines if next_steps.empty?
 
   this_step, *rest = next_steps
@@ -44,7 +44,7 @@ def split_timelines(timelines, next_steps)
     end
   end
 
-  split_timelines(new_timelines, rest)
+  split_timelines_grows_excessively!(new_timelines, rest)
 end
 
 def split(step, beam_pos)
@@ -57,12 +57,42 @@ def split(step, beam_pos)
   [timeline_one, timeline_two]
 end
 
+#####################################################################
+
+# THIRD ATTEMPT! I go back to the first recursive solution but build a 'memo'
+# of how many timelines are reachable from each beam position. As we revisit a
+# beam position a second time, we'll have the precomputed count of timelines
+# ready.
+def count_timelines(row, beam_position, rows, memo)
+  return memo[[row, beam_position]] if memo[[row, beam_position]]
+  return 1 if rows.empty?
+
+  this_level, *rest = rows
+  count = 0
+
+  if this_level[beam_position] == "^"
+    if beam_position > 0
+      # Beam splits to the left
+      count = count_timelines(row + 1, beam_position - 1, rest, memo)
+    end
+    if beam_position < this_level.size - 1
+      # Beam splits to the right
+      count += count_timelines(row + 1, beam_position + 1, rest, memo)
+    end
+  else
+    count = count_timelines(row + 1, beam_position, rest, memo)
+  end
+  memo[[row, beam_position]] = count
+  count
+end
+
 File.open("input", "r") do |file|
   rows = file.readlines.map(&:chomp).map(&:chars)
 
   first, *rest = rows
   first = first.map { |cell| cell == "S" ? "|" : cell }
+  beam_position = first.find_index("|")
 
-  result = split_timelines([first], rest)
-  puts result.size
+  result = count_timelines(0, beam_position, rest, {})
+  puts "Total timelines: #{result}"
 end
